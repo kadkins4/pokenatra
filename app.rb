2a1deba501e5029acb59d6414798efe18405e1d8
Kendall <adkins.kendall90@gmail.com>
2017-07-25 15:11:55 -0400

    minor setup changes

--- app.rb
+++ app.rb
@@ -2,9 +2,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
+require 'pry'
 
 #connect to db
 require_relative 'db/connection.rb'
 
 #Require models
 require_relative 'models/pokemon'
+
+get '/' do
+  
+end
+
+binding.pry
--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS pokes;
+DROP TABLE IF EXISTS pokemon;
 
-CREATE TABLE pokes (
+CREATE TABLE pokemon (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
   poke_type VARCHAR(255),
